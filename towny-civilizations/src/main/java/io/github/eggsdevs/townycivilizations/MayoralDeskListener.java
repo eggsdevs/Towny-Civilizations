259c736940c619c6853dfe28e3694a8850fc829b
Eric Nguyen <enguyen5435@gmail.com>
2025-08-07 15:11:01 -0500

    Added some stuff :)

--- towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/MayoralDeskListener.java
+++ towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/MayoralDeskListener.java
@@ -1,16 +1,23 @@
 package io.github.eggsdevs.townycivilizations;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Lectern;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -38,9 +45,11 @@ public class MayoralDeskListener implements Listener {
 
     private final Map<String, Location> desks = new HashMap<>();
     private final JavaPlugin plugin;
+    private final File storageFile;
 
-    public MayoralDeskListener(JavaPlugin plugin) {
+    public MayoralDeskListener(JavaPlugin plugin, File storageFile) {
         this.plugin = plugin;
+        this.storageFile = storageFile;
         Bukkit.getScheduler().runTaskTimer(plugin, () -> {
             for (Location loc : desks.values()) {
                 loc.getWorld().spawnParticle(Particle.ENCHANT, loc.clone().add(0.5, 0.75, 0.5), 20, 0.5, 0.5, 0.5, 0.0);
@@ -86,10 +95,14 @@ public class MayoralDeskListener implements Listener {
         String townName = town.getName();
         Location old = desks.get(townName);
         if (old != null) {
-            old.getBlock().setType(Material.AIR);
-            old.getWorld().spawnParticle(Particle.LARGE_SMOKE, old.clone().add(0.5, 0.5, 0.5), 20, 0.2, 0.2, 0.2, 0.0);
+            BlockState oldState = old.getBlock().getState();
+            if (oldState instanceof Lectern oldLectern) {
+                oldLectern.getInventory().clear();
+                oldLectern.update();
+            }
         }
         desks.put(townName, loc);
+        saveDesks();
 
         BlockState state = loc.getBlock().getState();
         if (state instanceof Lectern lectern) {
@@ -110,6 +123,7 @@ public class MayoralDeskListener implements Listener {
         Location loc = block.getLocation();
         if (desks.entrySet().removeIf(entry -> entry.getValue().equals(loc))) {
             loc.getWorld().spawnParticle(Particle.LARGE_SMOKE, loc.clone().add(0.5, 0.5, 0.5), 30, 0.3, 0.3, 0.3, 0.0);
+            saveDesks();
         }
     }
 
@@ -130,7 +144,25 @@ public class MayoralDeskListener implements Listener {
         }
 
         event.setCancelled(true);
-        Inventory gui = Bukkit.createInventory(null, 9, DESK_NAME);
+        Inventory gui = Bukkit.createInventory(null, 27, DESK_NAME);
+
+        String townName = desks.entrySet().stream()
+                .filter(entry -> entry.getValue().equals(loc))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse("Unknown");
+        Town town = TownyAPI.getInstance().getTown(townName);
+        String mayorName = town != null && town.getMayor() != null ? town.getMayor().getName() : "None";
+
+        ItemStack info = new ItemStack(Material.OAK_LOG);
+        var meta = info.getItemMeta();
+        if (meta != null) {
+            meta.setDisplayName(ChatColor.GOLD + townName);
+            meta.setLore(List.of(ChatColor.GRAY + "Mayor: " + ChatColor.GOLD + mayorName));
+            info.setItemMeta(meta);
+        }
+
+        gui.setItem(13, info);
         event.getPlayer().openInventory(gui);
     }
 
@@ -145,6 +177,56 @@ public class MayoralDeskListener implements Listener {
     public void onInventoryDrag(InventoryDragEvent event) {
         if (DESK_NAME.equals(event.getView().getTitle())) {
             event.setCancelled(true);
+        }
+    }
+
+    public Map<String, Location> getDesks() {
+        return desks;
+    }
+
+    public void loadDesks() {
+        desks.clear();
+        if (!storageFile.exists()) {
+            return;
+        }
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(storageFile);
+        ConfigurationSection section = config.getConfigurationSection("desks");
+        if (section == null) {
+            return;
+        }
+        for (String town : section.getKeys(false)) {
+            String worldName = section.getString(town + ".world");
+            World world = Bukkit.getWorld(worldName);
+            if (world == null) {
+                continue;
+            }
+            double x = section.getDouble(town + ".x");
+            double y = section.getDouble(town + ".y");
+            double z = section.getDouble(town + ".z");
+            Location loc = new Location(world, x, y, z);
+            desks.put(town, loc);
+            BlockState state = loc.getBlock().getState();
+            if (state instanceof Lectern lectern) {
+                lectern.getInventory().setItem(0, new ItemStack(Material.WRITABLE_BOOK));
+                lectern.update();
+            }
+        }
+    }
+
+    public void saveDesks() {
+        YamlConfiguration config = new YamlConfiguration();
+        for (Map.Entry<String, Location> entry : desks.entrySet()) {
+            Location loc = entry.getValue();
+            String path = "desks." + entry.getKey();
+            config.set(path + ".world", loc.getWorld().getName());
+            config.set(path + ".x", loc.getX());
+            config.set(path + ".y", loc.getY());
+            config.set(path + ".z", loc.getZ());
+        }
+        try {
+            config.save(storageFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.WARNING, "Failed to save mayoral desks", e);
         }
     }
 }
--- towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/TownyCivilizations.java
+++ towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/TownyCivilizations.java
@@ -1,9 +1,14 @@
 package io.github.eggsdevs.townycivilizations;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
@@ -15,11 +20,24 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class TownyCivilizations extends JavaPlugin {
 
+    private MayoralDeskListener mayoralDeskListener;
+    private File desksFile;
+
     @Override
     public void onEnable() {
         getLogger().info("TownyCivilizations enabled");
         registerMayoralDeskRecipe();
-        getServer().getPluginManager().registerEvents(new MayoralDeskListener(this), this);
+
+        File serverFolder = getDataFolder().getParentFile().getParentFile();
+        File dataFolder = new File(serverFolder, "Towny Civilizations");
+        if (!dataFolder.exists()) {
+            dataFolder.mkdirs();
+        }
+        desksFile = new File(dataFolder, "mayoral-desks.yml");
+
+        mayoralDeskListener = new MayoralDeskListener(this, desksFile);
+        mayoralDeskListener.loadDesks();
+        getServer().getPluginManager().registerEvents(mayoralDeskListener, this);
     }
 
     @Override
@@ -28,8 +46,53 @@ public class TownyCivilizations extends JavaPlugin {
             sender.sendMessage(ChatColor.GOLD + "Towny Civilizations" + ChatColor.GRAY + " adds civilization management features to Towny.");
             sender.sendMessage(ChatColor.YELLOW + "Develop your town into a thriving civilization!");
             return true;
+        } else if (command.getName().equalsIgnoreCase("mayoraldesks")) {
+            if (!sender.isOp() && !sender.hasPermission("townycivilizations.mayoraldesks")) {
+                sender.sendMessage(ChatColor.RED + "You don't have permission to use this command.");
+                return true;
+            }
+
+            int page = 1;
+            if (args.length > 0) {
+                try {
+                    page = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    sender.sendMessage(ChatColor.RED + "Invalid page number.");
+                    return true;
+                }
+            }
+
+            List<Map.Entry<String, Location>> entries = new ArrayList<>(mayoralDeskListener.getDesks().entrySet());
+            int perPage = 5;
+            int totalPages = (int) Math.ceil(entries.size() / (double) perPage);
+            if (totalPages == 0) {
+                totalPages = 1;
+            }
+            if (page < 1 || page > totalPages) {
+                sender.sendMessage(ChatColor.RED + "Page out of range. (1-" + totalPages + ")");
+                return true;
+            }
+
+            sender.sendMessage(ChatColor.GOLD + "Active Mayoral Desks - Page " + page + "/" + totalPages);
+            int start = (page - 1) * perPage;
+            int end = Math.min(start + perPage, entries.size());
+            for (int i = start; i < end; i++) {
+                var entry = entries.get(i);
+                Location loc = entry.getValue();
+                sender.sendMessage(ChatColor.YELLOW + entry.getKey() + ChatColor.GRAY + " at "
+                        + ChatColor.AQUA + loc.getWorld().getName() + " " + loc.getBlockX() + ", "
+                        + loc.getBlockY() + ", " + loc.getBlockZ());
+            }
+            return true;
         }
         return false;
+    }
+
+    @Override
+    public void onDisable() {
+        if (mayoralDeskListener != null) {
+            mayoralDeskListener.saveDesks();
+        }
     }
 
     private void registerMayoralDeskRecipe() {
