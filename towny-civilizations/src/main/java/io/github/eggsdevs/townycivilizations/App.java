03f49c98ee815ae2bc928f71b06fbac9fa62da24
Eric Nguyen <enguyen5435@gmail.com>
2025-08-06 22:23:18 -0500

    Adding basic testing features and Spigot API dependency

--- towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/App.java
+++ towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/App.java
@@ -1,13 +1,14 @@
 package io.github.eggsdevs.townycivilizations;
+import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App extends JavaPlugin{
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
     }
 }
