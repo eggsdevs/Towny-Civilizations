f8f0b511c85aeca5546be4dd9d042e569b859860
Eric Nguyen <enguyen5435@gmail.com>
2025-08-06 23:01:24 -0500

    Added the Mayoral Table

--- towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/TownyCivilizations.java
+++ towny-civilizations/src/main/java/io/github/eggsdevs/townycivilizations/TownyCivilizations.java
@@ -1,8 +1,14 @@
 package io.github.eggsdevs.townycivilizations;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -13,6 +19,7 @@ public class TownyCivilizations extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("TownyCivilizations enabled");
+        registerMayoralDeskRecipe();
     }
 
     @Override
@@ -23,5 +30,25 @@ public class TownyCivilizations extends JavaPlugin {
             return true;
         }
         return false;
+    }
+
+    /**
+     * Registers the crafting recipe for a special lectern named "Mayoral Desk".
+     */
+    private void registerMayoralDeskRecipe() {
+        ItemStack mayoralDesk = new ItemStack(Material.LECTERN);
+        ItemMeta meta = mayoralDesk.getItemMeta();
+        if (meta != null) {
+            meta.setDisplayName(ChatColor.GOLD + "Mayoral Desk");
+            mayoralDesk.setItemMeta(meta);
+        }
+
+        NamespacedKey key = new NamespacedKey(this, "mayoral_desk");
+        ShapedRecipe recipe = new ShapedRecipe(key, mayoralDesk);
+        recipe.shape("XXX", "XLX", "XXX");
+        recipe.setIngredient('X', Material.DIAMOND);
+        recipe.setIngredient('L', Material.LECTERN);
+
+        Bukkit.addRecipe(recipe);
     }
 }
